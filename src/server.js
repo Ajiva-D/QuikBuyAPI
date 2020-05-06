ad477526c4d26cebcb553ed33e41a7802ceaa395
Ajiva-D <davidenochaji@gmail.com>
2020-05-06 11:57:23 +0100

    finally configured babel

--- src/server.js
+++ src/server.js
@@ -1,16 +1,35 @@
-import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import morgan from 'morgan'
+// import express from 'express'
+var express = require('express')
+var { json, urlencoded } = require('body-parser')
+var morgan = require('morgan')
 // import config from './config'
-import cors from 'cors'
-import chalk from 'chalk'
+var cors =  require('cors')
+var chalk = require('chalk')
 
-export const app = express()
+const app = express()
 
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-const port = 1300;
-app.listen(port, () => console.log(`Server running on port ${chalk.green(port)}`));
+app.get('/', (req, res)=>{
+	res.send('Hello World!');
+})
+// const port = 1300;
+// app.listen(port, function () {
+// 	console.log(chalk.blue(`REST API on http://localhost:${port}/`))
+// });
+// const port = 1300;
+export const start = async () => {
+  try {
+		const port = 1300;
+    await app.listen(port, function () {
+			console.log(chalk.blue(`REST API on http://localhost:${port}/`))
+		});
+  } catch (e) {
+    console.error(e)
+  }
+}
+	
+// app.listen(port, () => console.log(`Server running on port ${chalk.green(port)}`));

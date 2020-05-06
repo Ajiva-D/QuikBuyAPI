826b171117541b65ba047f7685bb081d64283dd4
Ajiva-D <davidenochaji@gmail.com>
2020-05-06 12:55:49 +0100

    added model schema

---
+++ src/items/item.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const itemSchema = new mongoose.Schema(
+	{
+		img:{
+			type:String,
+			required: true,
+		},
+		title:{
+			type:String,
+			required:true
+		},
+		amount:{
+			type:Number,
+			required:true
+		},
+		rating:Number,
+		description:String
+
+	}
+)
